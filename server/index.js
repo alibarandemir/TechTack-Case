6d50e47e5091fdd39796f758b684fc93b6e03999
Ali Baran Demir <alibarandemir798@gmail.com>
2024-08-20 03:16:40 +0300

    server files updated

--- server/index.js
+++ server/index.js
@@ -7,15 +7,11 @@ const cors= require("cors")
 const app = express();
 const database=require('./config/database.js')
 app.use(cors())
-// Middleware
+
 app.use(express.json());
 database()
-// Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/posts', postRoutes);
-
-
-
 const PORT =  5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
